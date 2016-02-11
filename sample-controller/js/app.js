50d63f9c16c40d471a2900e0e337cca7d16412e5
kritika-pattalam <kritika.pattalam@gmail.com>
2016-02-10 22:13:35 -0500

    Initial Commit

---
+++ sample-controller/js/app.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+var app=angular.module('app',[]);
+
+
+app.controller('myCtrl', function() {
+    this.student = {name:"Volvo",
+                   age: 20};
+     
+
+
+
+});
