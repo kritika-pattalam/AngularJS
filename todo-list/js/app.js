77bb781e30b1d8f9730ddb64c7a6d6226dbc5a28
kritika-pattalam <kritika.pattalam@gmail.com>
2016-02-11 23:19:02 -0500

    Simple To-Do List with local storage

--- todo-list/js/app.js
+++ todo-list/js/app.js
@@ -3,27 +3,36 @@
 var app=angular.module('app',[]);
 //Define the controller
 app.controller('TodoCtrl', function($scope,$filter) {
-	//load with dummy list
-    $scope.todos=[{text:'learn angular',done:false},{text:'build angular',done:false}];
-    //displays the number of todo task available
-    $scope.totalTodos=$scope.todos.length;
-    //gets the number of tasks available for every task added
-    $scope.getTodoLength=function()
-    {
-    	$scope.totalTodos=$scope.todos.length;
-    }
+    //get saved to-do list from localstorage
+    $scope.saved = localStorage.getItem('todos');
+    //load local storage list, if it is empty load the default to-do list
+    $scope.todos = (localStorage.getItem('todos')!==null) ? JSON.parse($scope.saved) : [ {text: 'Learn AngularJS', done: false}, {text: 'Build an Angular app', done: false} ];
+	localStorage.setItem('todos', JSON.stringify($scope.todos));    
     //adds new task
     $scope.addTodo=function(){
     	$scope.todos.push({text:$scope.addText, done:false});
     	$scope.addText='';
+        localStorage.setItem('todos', JSON.stringify($scope.todos));
     	
     }
+    //tasks pending
+    $scope.tasksPending = function() {
+        var count = 0;
+        angular.forEach($scope.todos, function(todo){
+            count+= todo.done ? 0 : 1;
+        });
+        return count;
+    };
     //clears completed task
     $scope.clearCompleted=function(){
      	$scope.todos=$filter("filter")($scope.todos,function(list){
     		return !list.done;
     	})
+        localStorage.setItem('todos', JSON.stringify($scope.todos));
     }
-
-
+    //saves the changes made to checkbox,e.g . striking the task when its done
+    $scope.onCheckboxChange=function()
+    {
+        localStorage.setItem('todos', JSON.stringify($scope.todos));
+    }
 });
